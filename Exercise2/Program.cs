cc6615ccaa8ef08e737f604b44e8b849f19091fb
Baicus Andrei <baicus.andrei@yahoo.com>
2019-12-10 11:25:08 +0200

    Add project files.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = null;
+            string string2 = "hello";
+
+            Console.WriteLine("enter a string: ");
+            string1 = Console.ReadLine();
+            
+            if (string.IsNullOrEmpty(string1))
+            {
+                Console.WriteLine("string1 is null or empty.");
+            }
+            else
+            {
+                Console.WriteLine("string1 = " + string1);
+            }
+
+            if (string.Compare(string1.ToLower(), string2.ToLower()) == 0)
+            {
+                Console.WriteLine("Strings have the same value.");
+            }
+            else if (string.Compare(string1.ToLower(), string2.ToLower()) > 0)
+            {
+                Console.WriteLine("string1 > string2");
+            }
+            else
+            {
+                Console.WriteLine("string1 < string2");
+            }
+
+            string string3 = string.Concat(string2, string1);
+            string searchString = "world";
+
+            if (string3.Contains(searchString))
+            {
+                Console.WriteLine("string3 contains " + searchString);
+            }
+            else
+            {
+                Console.WriteLine("string3 don't contains " + searchString);
+            }
+
+            Console.WriteLine(string3.IndexOf(string1));
+
+            Console.WriteLine(string3.Trim().ToUpper());
+        }
+    }
+}
---
+++ HomeworkWeek3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkWeek3
+{
+    class Program
+    {
+        public enum Genders
+        {
+            Male,
+            Female,
+            Other
+        }
+
+        static void Main(string[] args)
+        {
+            int year, month, day;
+
+            Console.WriteLine("Enter your birth date");
+            Console.WriteLine("year: ");
+            String consoleString = Console.ReadLine();
+            year = int.Parse(consoleString);
+
+            Console.WriteLine("month: ");
+            consoleString = Console.ReadLine();
+            month = int.Parse(consoleString);
+
+            Console.WriteLine("day: ");
+            consoleString = Console.ReadLine();
+            day = int.Parse(consoleString);
+
+            DateTime birthDate = new DateTime(year, month, day);
+            Console.WriteLine(birthDate.ToString("dd/MM/yyyy"));
+
+            DateTime now = DateTime.Now;
+            TimeSpan diff = now - birthDate;
+
+            int age = (int)(diff.TotalDays / 365);
+            
+            Console.WriteLine("Gender(M/F): ");
+            consoleString = Console.ReadLine();
+            Genders gender;
+
+            if (consoleString.Equals("M") || consoleString.Equals("m"))
+            {
+                gender = Genders.Male;
+            }
+            else if (consoleString.Equals("F") || consoleString.Equals("f"))
+            {
+                gender = Genders.Female;
+            }
+            else
+            {
+                gender = Genders.Other;
+            }
+
+            if (gender != Genders.Other)
+            {
+                if (gender == Genders.Male)
+                {
+                    if (age < 65)
+                    {
+                        Console.WriteLine("You will retire at 65 years old.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are retired.");
+                    }
+                }
+
+                if (gender == Genders.Female)
+                {
+                    if (age < 63)
+                    {
+                        Console.WriteLine("You will retire at 63 years old.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are retired.");
+                    }
+                }
+            }
+                        
+            Console.WriteLine($"Your age is : {age}");
+
+        }
+    }
+}
